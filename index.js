4ccdef0f256e169a5866be3ce8d6f6293a8e65e4
ifty03 <ashikulislamb84@gmail.com>
2022-04-28 22:54:14 +0600

    connect server site with mongodb

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 /* all middleware */
 app.use(cors());
 app.use(express.json());
 
+/* all HTTP requests */
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjsdk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("conected");
+  // perform actions on the collection object
+  client.close();
 });
 
 app.listen(port, () => {
