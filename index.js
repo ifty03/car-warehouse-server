d38a7d7a26cd7ad0251d8032b9f12471dd7f771b
ifty03 <ashikulislamb84@gmail.com>
2022-04-29 09:07:40 +0600

    find one data from mongodb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 /* all middleware */
@@ -26,12 +26,20 @@ const run = async () => {
   try {
     await client.connect();
     const stokeCollection = client.db("stokedb").collection("stoke");
-    /* get data from database */
+    /* get all data from database */
     app.get("/stoke", async (req, res) => {
       const query = {};
       const cursor = stokeCollection.find(query).limit(6);
       const stokes = await cursor.toArray();
       res.send(stokes);
+    });
+
+    /* get one data from database */
+    app.use("/stoke/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await stokeCollection.findOne(query);
+      res.send(result);
     });
   } finally {
     // await client.close()
